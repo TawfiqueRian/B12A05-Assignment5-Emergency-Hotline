9de6f219ec3d6ea01cc45d0beae6565d7a07b2ff
TawfiqueRian <riantoufik897@gmail.com>
2025-08-27 14:09:48 +0600

    heart count functionality is done

--- js/index.js
+++ js/index.js
@@ -1,3 +1,32 @@
+//function to get heart count
 function heartCount(){
-    
+    const availableHeartCount = document.getElementById('heart-count').innerText;
+    const availableHeartCountNumber = parseInt(availableHeartCount);
+    return availableHeartCountNumber;
 }
+
+// function to set inner text 
+function setInnerHeartText(id, val){
+    document.getElementById(id).innerText = val;
+}
+
+//function to increase heart count
+function updateHeartCount(id)
+{
+    document.getElementById(id).addEventListener('click', function(){
+        const availableHeartCount = heartCount();
+        const newHeartCount = availableHeartCount + 1;
+        setInnerHeartText('heart-count', newHeartCount);
+    })
+}
+
+//updating heart count for all cards
+updateHeartCount('heart-btn1');
+updateHeartCount('heart-btn2');
+updateHeartCount('heart-btn3');
+updateHeartCount('heart-btn4');
+updateHeartCount('heart-btn5');
+updateHeartCount('heart-btn6');
+updateHeartCount('heart-btn7');
+updateHeartCount('heart-btn8');
+updateHeartCount('heart-btn9');
