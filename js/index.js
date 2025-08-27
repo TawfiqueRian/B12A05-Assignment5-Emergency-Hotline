7c538bfa350a0d5cb3a3487d7c5cd5bfb0259aef
TawfiqueRian <riantoufik897@gmail.com>
2025-08-27 19:38:42 +0600

    copy paste part is done

--- js/index.js
+++ js/index.js
@@ -46,60 +46,119 @@ updateHeartCount('heart-btn9', 'heart-count');
 // function of functionality of call button:
 const callData = [];
 
-function callButtonFunctionality(callBtnId, coinId, serviceId, phoneId){
-    document.getElementById(callBtnId).addEventListener('click', function(){
-        const availableCoin = getCount(coinId);
-        if(availableCoin < 20)
-        {
-            alert('You need minimum 20 coins to call');
-            return;
-        }
-        const newCoinCount = availableCoin - 20;
-        getAlert(serviceId, phoneId);
-        setInnerText('coin-count', newCoinCount);
-        const data = {
-            name: getInnerText(serviceId) ,
-            phone: getInnerText(phoneId),
-            date: new Date().toLocaleTimeString()
-        }
-        callData.push(data);
+function callButtonFunctionality(serviceId, phoneId){
+    const availableCoin = getCount('coin-count');
+    if(availableCoin < 20)
+    {
+        alert('You need minimum 20 coins to call');
+        return;
+    }
+    const newCoinCount = availableCoin - 20;
+    getAlert(serviceId, phoneId);
+    setInnerText('coin-count', newCoinCount);
+    const data = {
+        name: getInnerText(serviceId) ,
+        phone: getInnerText(phoneId),
+        date: new Date().toLocaleTimeString()
+    }
+    callData.push(data);
 
-        const parentNode = document.getElementById('history-container');
-        parentNode.innerText = '';
-        for(const elData of callData)
-        {
-            const newBox = document.createElement('div');
-            newBox.innerHTML = `
-                <div class="history-box flex justify-between items-center bg-gray-50 px-[10px] py-[16px] rounded-[8px] mb-[8px]">
-                    <div>
-                        <h2 class="text-[0.75rem] font-semibold">${elData.name}</h2>
-                        <p class="text-[0.70rem] text-gray-500">${elData.phone}</p>
-                    </div>
-                    <div>
-                        <p class="text-[0.80rem] text-gray-500">${elData.date}</p>
-                    </div>
+    const parentNode = document.getElementById('history-container');
+    parentNode.innerText = '';
+    for(const elData of callData)
+    {
+        const newBox = document.createElement('div');
+        newBox.innerHTML = `
+            <div class="history-box flex justify-between items-center bg-gray-50 px-[10px] py-[16px] rounded-[8px] mb-[8px]">
+                <div>
+                    <h2 class="text-[0.75rem] font-semibold">${elData.name}</h2>
+                    <p class="text-[0.70rem] text-gray-500">${elData.phone}</p>
                 </div>
-            `
-            parentNode.appendChild(newBox);
-        }
-    })
+                <div>
+                    <p class="text-[0.80rem] text-gray-500">${elData.date}</p>
+                </div>
+            </div>
+        `
+        parentNode.appendChild(newBox);
+    }
 }
 
 //functionality of call buttons
-callButtonFunctionality('call-btn1', 'coin-count', 'service1', 'phone1');
-callButtonFunctionality('call-btn2', 'coin-count', 'service2', 'phone2');
-callButtonFunctionality('call-btn3', 'coin-count', 'service3', 'phone3');
-callButtonFunctionality('call-btn4', 'coin-count', 'service4', 'phone4');
-callButtonFunctionality('call-btn5', 'coin-count', 'service5', 'phone5');
-callButtonFunctionality('call-btn6', 'coin-count', 'service6', 'phone6');
-callButtonFunctionality('call-btn7', 'coin-count', 'service7', 'phone7');
-callButtonFunctionality('call-btn8', 'coin-count', 'service8', 'phone8');
-callButtonFunctionality('call-btn9', 'coin-count', 'service9', 'phone9');
+document.getElementById('call-btn1').addEventListener('click', function(){
+    callButtonFunctionality('service1', 'phone1');
+})
+document.getElementById('call-btn2').addEventListener('click', function(){
+    callButtonFunctionality('service2', 'phone2');
+})
+document.getElementById('call-btn3').addEventListener('click', function(){
+    callButtonFunctionality('service3', 'phone3');
+})
+document.getElementById('call-btn4').addEventListener('click', function(){
+    callButtonFunctionality('service4', 'phone4');
+})
+document.getElementById('call-btn5').addEventListener('click', function(){
+    callButtonFunctionality('service5', 'phone5');
+})
+document.getElementById('call-btn6').addEventListener('click', function(){
+    callButtonFunctionality('service6', 'phone6');
+})
+document.getElementById('call-btn7').addEventListener('click', function(){
+    callButtonFunctionality('service7', 'phone7');
+})
+document.getElementById('call-btn8').addEventListener('click', function(){
+    callButtonFunctionality('service8', 'phone8');
+})
+document.getElementById('call-btn9').addEventListener('click', function(){
+    callButtonFunctionality('service9', 'phone9');
+})
 
 //funcionality of clear button 
 
 document.getElementById('clear-btn').addEventListener('click', function(){
     const boxes = document.getElementById('history-container');
-    boxes.innerHTML = '';
+    boxes.innerText = '';
     callData.length = 0;
+})
+
+
+
+// function of functionality of copy button
+function copyBtnFunctionality(serviceId, phoneId){
+    alert('Hotline number of ' + document.getElementById(serviceId).innerText + ' is coppied');
+    const availableCopy = getCount('copy-count');
+    const newCopyCount = availableCopy + 1;
+    setInnerText('copy-count', newCopyCount);
+
+    const copiedText = document.getElementById(phoneId).innerText;
+    navigator.clipboard.writeText(copiedText);
+}
+
+//functionality of copy button
+
+document.getElementById('copy-btn1').addEventListener('click', function(){
+    copyBtnFunctionality('service1', 'phone1');
+})
+document.getElementById('copy-btn2').addEventListener('click', function(){
+    copyBtnFunctionality('service2', 'phone2');
+})
+document.getElementById('copy-btn3').addEventListener('click', function(){
+    copyBtnFunctionality('service3', 'phone3');
+})
+document.getElementById('copy-btn4').addEventListener('click', function(){
+    copyBtnFunctionality('service4', 'phone4');
+})
+document.getElementById('copy-btn5').addEventListener('click', function(){
+    copyBtnFunctionality('service5', 'phone5');
+})
+document.getElementById('copy-btn6').addEventListener('click', function(){
+    copyBtnFunctionality('service6', 'phone6');
+})
+document.getElementById('copy-btn7').addEventListener('click', function(){
+    copyBtnFunctionality('service7', 'phone7');
+})
+document.getElementById('copy-btn8').addEventListener('click', function(){
+    copyBtnFunctionality('service8', 'phone8');
+})
+document.getElementById('copy-btn9').addEventListener('click', function(){
+    copyBtnFunctionality('service9', 'phone9');
 })
